683df84e47a4ff651699ea6cc879eae6190974fe
om <omkorde.rs2002@gmail.com>
2022-12-20 06:35:08 +0530

    Initial Commit

---
+++ index.py
@@ -0,0 +1,48 @@
+def sum(a,b,c):
+    return a+b+c
+def printboard(xstate,zstate):
+    zero= "X" if xstate[0] else("O" if zstate[0] else 0)
+    one="X" if xstate[1] else("O" if zstate[1] else 1)
+    two= "X" if xstate[2] else("O" if zstate[2] else 2)
+    three="X" if xstate[3] else("O" if zstate[3] else 3)
+    four= "X" if xstate[4] else("O" if zstate[4] else 4)
+    five="X" if xstate[5] else("O" if zstate[5] else 5)
+    six= "X" if xstate[6] else("O" if zstate[6] else 6)
+    seven="X" if xstate[7] else("O" if zstate[7] else 7)
+    eight= "X" if xstate[8] else("O" if zstate[8] else 8)
+    print(f"{zero} |{one} |{two}")
+    print(f"--|--|--")
+    print(f"{three} |{four} |{five}")
+    print(f"--|--|--")
+    print(f"{six} |{seven} |{eight}")
+def checkwin(xstate,zstate):
+    wins=[[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[2,4,6]]
+    for win in wins:
+        if(sum(xstate[win[0]],xstate[win[1]],xstate[win[2]])==3):
+            print("x is win ")
+            return 1
+        if(sum(zstate[win[0]],zstate[win[1]],zstate[win[2]])==3):
+            print("o is win ")
+            return 0
+    return -1
+xstate=[0,0,0,0,0,0,0,0,0]
+zstate=[0,0,0,0,0,0,0,0,0]
+print("Welcome in Tic Tac Toe")
+turn=1
+count=0
+while(True and count<9):
+    printboard(xstate,zstate)
+    if(turn==1):
+        print("x's chance ")
+        value=int(input("Enter the value for to play x chance"))
+        xstate[value]=1
+    else:
+        print("o's chance")
+        value=int(input("Enter the value for to play o chance"))
+        zstate[value]=1
+    check=checkwin(xstate,zstate)
+    if(check!=-1):
+        print("Match is over")
+        break
+    turn=1-turn
+    count=count+1
